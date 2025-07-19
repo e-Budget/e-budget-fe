ae9cba8a995244782e130eeace8cd32a355b883d
Leandro Pereira <leojgpereira@gmail.com>
2025-07-19 16:15:40 +0100

    [FEAT] Add navbar and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {Dashboard} from './features/dashboard/components/dashboard/dashboard';
+import {Overview} from './features/overview/components/overview/overview';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Overview
+  }
+];

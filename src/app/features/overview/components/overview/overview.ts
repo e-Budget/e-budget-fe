e5989985d730abe5835ffda310ce93302364cbe6
Leandro Pereira <leojgpereira@gmail.com>
2025-07-20 11:33:34 +0100

    [FEAT] Create Header component

--- src/app/features/overview/components/overview/overview.ts
+++ src/app/features/overview/components/overview/overview.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {Navbar} from '../../../../shared/components/navbar/navbar';
 import {Footer} from '../../../../shared/components/footer/footer';
+import {Header} from '../../../../shared/components/header/header';
 
 @Component({
   selector: 'app-overview',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Header, Footer],
   templateUrl: './overview.html',
   styleUrl: './overview.scss'
 })

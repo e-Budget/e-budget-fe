f36983ebaf2c815d1148e4ffb1c8487efa87d069
Leandro Pereira <64182156+leojgpereira@users.noreply.github.com>
2025-07-26 11:33:15 +0100

    [FEAT] Add analysis component (#8)

--- src/app/features/overview/components/overview/overview.ts
+++ src/app/features/overview/components/overview/overview.ts
@@ -7,10 +7,11 @@ import {QuickActions} from './quick-actions/quick-actions';
 import {KpiCard} from './kpi-card/kpi-card';
 import {Accounts} from './accounts/accounts';
 import {Budget} from './budget/budget';
+import {Analysis} from './analysis/analysis';
 
 @Component({
   selector: 'app-overview',
-  imports: [Navbar, Header, Content, KpiCard, Accounts, Budget, QuickActions, Footer],
+  imports: [Navbar, Header, Content, KpiCard, Accounts, Analysis, Budget, QuickActions, Footer],
   templateUrl: './overview.html',
   styleUrl: './overview.scss'
 })
